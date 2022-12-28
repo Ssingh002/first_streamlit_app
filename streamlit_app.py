629024fb9955576181aadac2442b87c41d29ff81
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2022-12-28 16:15:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text ('🥑🍞Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
   
-//andas is another pacakage library. File below is picked from our s3 bucket
+--andas is another pacakage library. File below is picked from our s3 bucket
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)    

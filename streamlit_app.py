2a73ba10e0d5f66e18bc49bedd753a3447b8e390
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2022-12-29 09:39:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #Pandas is another pacakage library. File below is picked from our s3 bucket
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)    
+#my_fruite_list is called a dataframe
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page.
+
+streamlit.dataframe(my_fruit_list)   
+# display dataframe

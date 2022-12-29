f697cff077c37a630d86077063aec29f48a093a3
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2022-12-29 09:56:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,13 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #my_fruite_list is called a dataframe
 my_fruit_list = my_fruit_list.set_index('Fruit')
+#above allows you to pick the specific fruit from drop down. Without this we would have to use the rowID
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# Display the table on the page.
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page
+streamlit.multiselect ("Pick some fruits:",'list(my_fruit_list.index),['Acavado','Strawberries'])
+#Above prepopulates listed fruite
 
 streamlit.dataframe(my_fruit_list)   
 # display dataframe

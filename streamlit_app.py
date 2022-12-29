d1a2f5866cc073687bcbf47455053b4c48a73e00
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2022-12-29 10:12:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page
-streamlit.multiselect ("Pick some fruits:",list(my_fruit_list.index),['Avacado','Lemon'])
+streamlit.multiselect ("Pick some fruits:",list(my_fruit_list.index),['Avacado'])
 #Above prepopulates listed fruite
 #Important: Fruit name needs to be spelled the same as the dataframe. Error otherwise
 

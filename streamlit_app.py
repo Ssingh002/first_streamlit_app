099d5a1bf80841c1eae41b86157dda9a097e648a
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2023-01-18 15:23:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # dumps the variable
 streamlit.dataframe(fruityvice_normalized)
 
+#Lesson 12. Created new requirements.txt
 import snowflake.connector
+# query trail a/c metadata
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)

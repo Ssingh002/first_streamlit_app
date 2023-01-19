aaea3d5ec268f90ea6d5840a073b79abc40837be
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2023-01-18 18:20:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,10 @@ my_cur = my_cnx.cursor()
 my_cur.execute ("select * from fruit_load_list")
 my_data_row = my_cur.fetchone() 
 # above fetches one using cursor
-streamlit.text ("The fruit load list contains:")
+streamlit.header ("The fruit load list contains:")
 streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
+
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")

34c80e2e4a98fb2124087b4ae43aa6d4f6954d8c
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2023-01-19 09:34:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,8 @@ except URLError as e:
     streamlit.error()
     
 #New section to display fruitvice api response
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi") 

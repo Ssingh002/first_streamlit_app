4f036e99f63fe4d21ea548ee06f8e55c64fe7d4a
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2023-01-18 18:04:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,10 @@ import snowflake.connector
 # query trail a/c metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+my_cur.execute ("select * from fruit_load_list")
+my_data_row = my_cur.fetchone() # fetches one using cursor
+streamlit.text(my_data_row)
+stremlit.text ("The fruit load list contains:")
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")

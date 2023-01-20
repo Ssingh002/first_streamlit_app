5e994162ae48c1d18d7b6cfda99198e567ec7ab8
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2023-01-20 10:41:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,7 +127,7 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 
 #this will not work correctly
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 
 
 

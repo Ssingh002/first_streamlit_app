20d06e68bb23d8e73fbcc1fef3abcb8490b9acc6
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2023-01-20 15:11:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,8 +118,8 @@ if streamlit.button('Get Fruit Load list'):
 
 
 #New section to display fruitvice api response
-add_my_fruit = streamlit.text_input('What fruit would you like like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like like to add?','jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
 
 #this will not work correctly

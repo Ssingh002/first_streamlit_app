59fa56b3e92805411734dce59f759bb0f94925b8
Sujoy Singh <121317550+Ssingh002@users.noreply.github.com>
2023-01-20 10:19:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,22 +104,31 @@ if streamlit.button('Get Fruit Load list'):
 
 #would put all the data into normalized format - column/row
 
-
-
-streamlit.stop()
-
-
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-
-
+#L12: Use a Function and Button to ADd the Fruit Name Submissions
 #New section to display fruitvice api response
-add_my_fruit = streamlit.text_input('What fruit would you like like to add?','jackfruit')
+#Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_Load_list values ('from streamlit')")
+    return "Thanks for adding" + new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+  
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 
 #this will not work correctly
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+
+
+
+streamlit.stop()
